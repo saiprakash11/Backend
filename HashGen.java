8065ae243cfd27328579e63d0926aec1c7579baa
NITHIN <workhardtest.2005@gmail.com>
2026-07-04 11:19:16 +0530

    Delete HashGen.java

--- HashGen.java
+++
@@ -1,2 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-public class HashGen { public static void main(String[] args) { System.out.println(new BCryptPasswordEncoder().encode("Admin@123")); } }
